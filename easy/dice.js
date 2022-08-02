ad00c270b8034c9894c04a09f744b99f58f46324
Simon Love <simon-love@hotmail.co.uk>
2022-08-02 15:17:58 +0100

    02/08 1st commit

--- easy/dice.js
+++ easy/dice.js
@@ -1,6 +1,25 @@
 // write your classes:
 
+class Die{
 
+    constructor(sides){
+        this.sides = sides
+    }
+
+    roll(){
+        return Math.floor(Math.random()*this.sides)+1
+    }
+}
+
+
+var myDie = new Die (6)
+console.log("My dice has " + myDie.sides + " sides!")
+// myDie.roll()
+
+// class Cup{
+
+
+// }
 
 // test your classes with console.log
 // to show they work as intended:
